bbad382479bf8374400628f2285c9964629f6926
Sangareshwaran <sangareshwarannadar@gmail.com>
2024-06-26 22:32:03 +0530

    Some changes

--- planner.js
+++ planner.js
@@ -1,52 +1,62 @@
-
-
-
-function make(data) 
-{
+async function make(data) {
+  console.log(data);
   var card = document.createElement('div');
-  card.className = "card" ;
-  
+  card.className = "card";
+
   var title = document.createElement('h1');
   title.textContent = 'Furina';
   card.appendChild(title);
-  
+
   var todo = document.createElement('div');
   todo.className = 'todo';
   var img = document.createElement('img');
-  img.src = './images/charecters/'+'furina'+'/icon.webp';
-  todo.appendChild(document.createElement('div').appendChild(img));
+  img.src = './images/characters/' + 'furina' + '/icon.webp';
   
+  var imgContainer = document.createElement('div');
+  imgContainer.appendChild(img);
+  todo.appendChild(imgContainer);
+
   var progress_bars = document.createElement('div');
   progress_bars.className = 'progress_bars';
-  
-  for (let material in data['materials'])
-    {
-      var item = document.createElement('div');
-      item.className = 'item';
-      var img = document.createElement('img');
-      img.src = 'https://raw.githubusercontent.com/Sangareshwaran/Quqi-Crits/main/images/items/'+ material + '.webp'
-      item.appendChild(img)
-      item.appendChild(document.createElement('h4').textContent = material.split('-')[0])
-      
-      todo.appendChild(item)
-      todo.appendChild(document.createElement('hr'))
-      console.log(item)
-    }
-    
-    card.appendChild(todo);
-    document.getElementsByClassName("container")[0].appendChild(card);
+
+  for (let material in data['materials']) {
+    var item = document.createElement('div');
+    item.className = 'item';
+    var materialImg = document.createElement('img');
+    materialImg.src = 'https://raw.githubusercontent.com/Sangareshwaran/Quqi-Crits/main/images/items/' + material + '.webp';
+    item.appendChild(materialImg);
+
+    var materialName = document.createElement('h4');
+    materialName.textContent = material.split('-')[0];
+    item.appendChild(materialName);
+
+    todo.appendChild(item);
+    todo.appendChild(document.createElement('hr'));
+    console.log("hello?");
   }
-  
+
+  card.appendChild(todo);
+  document.getElementsByClassName("container")[0].appendChild(card);
+}
+
 async function read_json(path_file) {
-      try {
-        const response = await fetch(path_file);
-        if (!response.ok) {
-          throw new Error('Network response was not ok ' + response.statusText);
-        }
-        const data = await response.json();
-        console.log(data); // Process the JSON data
-      } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
-      }
+  try {
+    const response = await fetch(path_file);
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
     }
-make(read_json('https://raw.githubusercontent.com/Sangareshwaran/Quqi-Crits/main/data/furina.json'));
+    const data = await response.json();
+    return data; // Process the JSON data
+  } catch (error) {
+    console.error('There has been a problem with your fetch operation:', error);
+  }
+}
+
+async function init() {
+  const data = await read_json('https://raw.githubusercontent.com/Sangareshwaran/Quqi-Crits/main/data/furina.json');
+  if (data) {
+    make(data);
+  }
+}
+
+init();
